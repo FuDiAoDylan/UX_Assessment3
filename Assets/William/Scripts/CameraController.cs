d21f15afd268596a1102c50ae70bdf4f9e8b756c
xiaoliandan <1921547406@qq.com>
2022-04-20 01:08:56 +0800

    Game can run normally
    The game can run normally. If the character eats up all the food, the game wins.If the character falls into a trap, the game ends.

---
+++ Assets/William/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+﻿// Reference https://python.iitter.com/other/315447.html
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+
+    Vector3 off;     //相机目标点位置信息
+    public float speed;   //相机移动速度
+    Vector3 ve;     //平滑阻尼的返回值
+    Quaternion angel;   //相机看向目标的旋转值
+    public float hight;   //相机的高度
+    public float foward;  //相机在角色后的距离
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // transform.position = player.transform.position + offset;
+
+        off = player.transform.position + hight * player.transform.up - foward * player.transform.forward;
+        // off = new Vector3(0,1.9f,0.15f);
+        // transform.position = player.transform.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, off, ref ve, 0);
+        //看向向量指向的方向
+        angel = Quaternion.LookRotation(player.transform.position - off);
+        transform.rotation = Quaternion.Slerp(transform.rotation, angel, Time.deltaTime * speed);
+    }
+}
---
+++ Assets/William/Scripts/PickUp.cs
@@ -0,0 +1,72 @@
+// Reference https://learn.unity.com/tutorial/displaying-score-and-text?uv=2019.4&projectId=5f158f1bedbc2a0020e51f0d#
+
+using UnityEngine;
+
+// Include the namespace required to use Unity UI and Input System
+using TMPro;
+
+public class PickUp : MonoBehaviour
+{
+
+	public TextMeshProUGUI countText;
+	public GameObject winImageObject;
+	public GameObject failImageObject;
+
+	public AudioSource pickUpAudio;
+	public AudioSource winAudio;
+	public AudioSource failAudio;
+	public AudioSource backAudio;
+
+	private int count;
+
+	// At the start of the game..
+	void Start()
+	{
+		// Set the count to zero 
+		count = 0;
+		SetCountText();
+
+		// Set the text property of the Win Text UI to an empty string, making the 'You Win' (game over message) blank
+		winImageObject.SetActive(false);
+		failImageObject.SetActive(false);
+	}
+
+	void FixedUpdate()
+	{
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		// and if the GameObject you intersect has the tag 'Pick Up' assigned to it..
+		if (other.gameObject.CompareTag("food"))
+		{
+			other.gameObject.SetActive(false);
+			pickUpAudio.Play();
+			// Add one to the score variable 'count'
+			count = count + 1;
+
+			// Run the 'SetCountText()' function (see below)
+			SetCountText();
+		}
+		if (other.gameObject.CompareTag("enemy"))
+		{
+			failImageObject.SetActive(true);
+			backAudio.Stop();
+			failAudio.Play();	
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString();
+
+		if (count >= 6)
+		{
+			// Set the text value of your 'winText'
+			winImageObject.SetActive(true);
+			backAudio.Stop();
+			winAudio.Play();
+		}
+	}
+}
